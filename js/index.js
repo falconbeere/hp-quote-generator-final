ccbc55a4b20e1f3b9d0abc8efb350a2a7c4bbd25
falconbeere <robyn_ivy@hotmail.com>
2016-06-08 13:00:50 -0700

    meh

--- js/index.js
+++ js/index.js
@@ -21,7 +21,7 @@
 	    	name:"Severus Snape"
 	    },
 	    {
-	    	quote:"Of course it is happen inside your head, but why on earth should that mean that it is not real?",
+	    	quote:"Of course it is happening inside your head, but why on earth should that mean that it is not real?",
 	    	name:"Albus Dumbledore"
 	    },
 	    {
@@ -104,36 +104,6 @@
 	];
 		
 
-		$('#quoteButton').click(function(evt){
-			//define the containers of the info we target
-			var quote = $('#quoteContainer p').text();
-			var quoteGenius = $('#quoteGenius').text();
-			//prevent browser's default action
-			evt.preventDefault();
-			//getting a new random number to attach to a quote and setting a limit
-			var sourceLength = quoteSource.length;
-			var randomNumber= Math.floor(Math.random()*sourceLength);
-			//set a new quote
-			for(i=0;i<=sourceLength;i+=1){
-			var newQuoteText = quoteSource[randomNumber].quote;
-			var newQuoteGenius = quoteSource[randomNumber].name;
-			//console.log(newQuoteText,newQuoteGenius);
-      var timeAnimation = 500;
-      var quoteContainer = $('#quoteContainer');
-      //fade out animation with callback
-      quoteContainer.fadeOut(timeAnimation, function(){
-        quoteContainer.html('');
-				quoteContainer.append('<p>'+newQuoteText+'</p>'+'<p id="quoteGenius">'+'-								'+newQuoteGenius+'</p>');
-        
-        //fadein animation.
-        quoteContainer.fadeIn(timeAnimation);
-      });  
-			
-			break;
-		};//end for loop
-	
-	});//end quoteButton function
-		
 		$('html').click(function(evt){
 			//define the containers of the info we target
 			var quote = $('#quoteContainer p').text();
@@ -163,5 +133,35 @@
 		};//end for loop
 	
 	});//end quoteButton function
+		
+    		$('#quoteButton').click(function(evt){
+			//define the containers of the info we target
+			var quote = $('#quoteContainer p').text();
+			var quoteGenius = $('#quoteGenius').text();
+			//prevent browser's default action
+			evt.preventDefault();
+			//getting a new random number to attach to a quote and setting a limit
+			var sourceLength = quoteSource.length;
+			var randomNumber= Math.floor(Math.random()*sourceLength);
+			//set a new quote
+			for(i=0;i<=sourceLength;i+=1){
+			var newQuoteText = quoteSource[randomNumber].quote;
+			var newQuoteGenius = quoteSource[randomNumber].name;
+			//console.log(newQuoteText,newQuoteGenius);
+      var timeAnimation = 500;
+      var quoteContainer = $('#quoteContainer');
+      //fade out animation with callback
+      quoteContainer.fadeOut(timeAnimation, function(){
+        quoteContainer.html('');
+				quoteContainer.append('<p>'+newQuoteText+'</p>'+'<p id="quoteGenius">'+'-								'+newQuoteGenius+'</p>');
         
+        //fadein animation.
+        quoteContainer.fadeIn(timeAnimation);
+      });  
+			
+			break;
+		};//end for loop
+	
+	});//end quoteButton function
+		
 });//end document ready
